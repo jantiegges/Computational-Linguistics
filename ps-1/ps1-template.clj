755b8485f5a8b58c9a90e1fc1e9da8bc23c16f5c
jantiegges <jantiegges@gmail.com>
2023-09-08 13:55:23 -0400

    first commit

---
+++ ps-1/ps1-template.clj
@@ -0,0 +1,48 @@
+;; Problem 1
+;; Define a variable `year`
+
+
+;; Problem 5
+;; Define a function `add-up`
+
+
+;; Problem 6
+;; Define a function `is-it-four?` 
+;;   (Note: Don't forget the question mark in the name!)
+
+
+;; Problem 7
+; add your code in the indicated space below,
+; (so that the symbol `problem-7` evaluates to `true`)
+
+(def problem-7 ;<-- do not edit this
+		(= (quote    ;<-- do not edit this
+		
+		; ~~ your code here! ~~
+
+		) 'platypus) ;<-- do not edit this
+)              ;<-- do not edit this
+
+
+;; Problem 8
+;; Define a function `func` and an expression `expr` 
+
+
+;; Problem 9
+;; Define a function `both-same-type?`
+;;   (Note: Don't forget the question mark in the name!)
+
+;; Problem 10
+;; Define a function `list-longer-than?`
+;;   (Note: Don't forget the question mark in the name!)
+
+;; Problem 11
+;; Define a function `dot-product`
+
+
+;; Problem 12
+;; Define a function `swap-arg-order`
+
+
+;; Problem 13
+;; Define a higher order function `g`
