edc0513148e135315ab56d07c30cb9798c454996
jantiegges <jantiegges@gmail.com>
2023-09-13 20:31:08 -0400

    ps-2 initial files

---
+++ ps-2/ps2-template.clj
@@ -0,0 +1,53 @@
+
+;; Problem 1: define a function `absval`
+
+
+;; Problem 2: fix the functions `take-square` and `sum-of-squares`
+
+
+;; Problem 3: define expressions `exp-13-1`, `exp-13-2`, `exp-13-3`, and `exp-13-4`
+
+
+;; Problem 4: define a function `third`
+
+
+;; Problem 5:
+;; [let's import define `sqrt` and `abs` from Java.lang.Math for the example]
+(defn sqrt [x] (Math/sqrt x))
+(defn abs [x] (Math/abs x))
+;; define a function `compose` 
+
+
+;; Problem 6: define a function `first-two`
+
+
+;; Problem 7: define a function `remove-second`
+
+
+;; Problem 8: define a function `add-to-end`
+
+
+;; Problem 9: define a function `reverse`
+;;           (Note that this funciton will overwrite a built in function. 
+;;            This is okay.)
+
+
+;; Problem 10: define a function `count-to-1`
+
+
+;; Problem 11: define a function `count-to-n`
+
+
+;; Problem 12: define a function `get-max`
+
+
+;; Problem 13: define a function `greater-than-five?`
+
+
+;; Problem 14: define a function `concat-three`
+
+
+;; Problem 15: define a function `sequence-to-power`
+
+
+;; Problem 16: define a function `in-L-star?`
