b1ef57f038253398b1edbc6af6b5973479a7768e
jantiegges <jantiegges@gmail.com>
2023-09-26 17:22:10 -0400

    final submission

--- ps-2/ps2-test.clj
+++ ps-2/ps2-test.clj
@@ -1,4 +1,3 @@
-
 ;; Problem 1: define a function `absval` 
 (defn absval [x] (Math/sqrt (* x x)))
 (absval -3)
@@ -10,18 +9,18 @@
 (sum-of-squares 3 4)
 
 ;; Problem 3: define expressions `exp-13-1`, `exp-13-2`, `exp-13-3`, and `exp-13-4`
-(def exp-13-1 '(+ 4 4 5))
-(def exp-13-2 '(+ 12 1))
-(def exp-13-3 '(+ 1 1 1 1 1 1 1 1 1 1 1 1 1))
-(def exp-13-4 '(- 13 0))
-(+ 4 4 5)
-(+ 12 1)
-(+ 1 1 1 1 1 1 1 1 1 1 1 1 1)
-(- 13 0)
+(def exp-13-1 '(- 13 0))
+(def exp-13-2 '(* 2 6.5))
+(def exp-13-3 '(/ 26 2))
+(def exp-13-4 '(Math/sqrt 169))
+(eval exp-13-1)
+(eval exp-13-2)
+(eval exp-13-3)
+(eval exp-13-4)
 
 ;; Problem 4: define a function `third`
 (defn third [x] (first (rest (rest x))))
-(third [1 2 3 4 5])
+(third [1 2])
 
 
 ;; Problem 5:
@@ -33,53 +32,82 @@
 ((compose sqrt abs) -36)
 
 ;; Problem 6: define a function `first-two`
-(defn first-two [l] (take 2 l))
-(first-two [1 2 3 4 5])
+(defn first-two [l] (list (first l) (first (rest l))))
+(first-two [1 2 3])
 
 ;; Problem 7: define a function `remove-second`
-(defn remove-second [l] (concat (take 1 l) (drop 2 l)))
+(defn remove-second [l] (cons (first l) (rest (rest l))))
 (remove-second [1 2 3 4 5])
 
 ;; Problem 8: define a function `add-to-end`
-(defn add-to-end [lst x] (concat lst (list x)))
+(defn add-to-end [lst x]
+  (if (empty? lst)
+    (list x)
+    (cons (first lst) (add-to-end (rest lst) x))))
 (add-to-end (list 5 6 4) 0)
 
 ;; Problem 9: define a function `reverse`
 ;;           (Note that this funciton will overwrite a built in function. 
 ;;            This is okay.)
-(defn reverse-list [lst] (reduce conj '() lst))
+(defn reverse-list [lst] 
+  (if (empty? lst)
+    '()
+    (concat (reverse-list (rest lst)) (list (first lst)))))
 (reverse-list [1 2 3 4])
-;; allowed as higher-order function?
 
 ;; Problem 10: define a function `count-to-1`
-(defn count-to-1 [n] (reverse-list (take n (iterate inc 1))))
-(take 3 (iterate inc 1))
-(count-to-1 3)
+(defn count-to-1 [n] 
+  (if (<= n 1) 
+     (list 1) 
+     (cons n (count-to-1 (- n 1))     
+)))
+(count-to-1 1)
 
 ;; Problem 11: define a function `count-to-n` in a different way
-(defn count-to-n [n] (take n (iterate inc 1)))
-
+(defn count-to-n [n] (reverse-list (count-to-1 n)))
+(count-to-n 5)
 
 ;; Problem 12: define a function `get-max` without using max and reduce. do not use max and reduce. no reduce. no max.
-(defn get-max [lst] (last (sort lst)))
-
+(defn get-max [lst]
+  (if (empty? lst)
+    nil
+    (if (empty? (rest lst))
+      (first lst)
+        (if (> (first lst) (get-max (rest lst)))
+          (first lst)
+          (get-max (rest lst))))))
+(get-max '(7 2 5 3 10))
 
 ;; Problem 13: define a function `greater-than-five?`
-(> 6 5)
 (defn greater-than-five? [lst] (map (fn [x] (> x 5)) lst))
 (greater-than-five? (list 5 4 7))
 
 ;; Problem 14: define a function `concat-three`
-(defn concat-three [x y z] (concat x y z))
-(concat-three [1 2 3] [4 5 6] [7 8 9])
+(defn concat-three [x y z]
+  (if (empty? x)
+    (if (empty? y)
+      z
+      (cons (first y) (concat-three x (rest y) z)))
+    (cons (first x) (concat-three (rest x) y z))))
+(concat-three (list 'a 'b) (list 'b 'c)  (list 'd 'e))
 
 ;; Problem 15: define a function `sequence-to-power`
-(defn sequence-to-power [x n] (reduce concat '() (take n (iterate (fn [x] x) x))))
+(defn sequence-to-power [x n] 
+  (if (empty? x)
+    '()
+    (if (= n 0)
+      '()
+      (if (= n 1)
+        x
+        (concat x (sequence-to-power x (- n 1)))))))
 (sequence-to-power (list 'a 'b) 3)
 
 ;; Problem 16: define a function `in-L-star?`
 (def L '(a))
-(defn in-L-star? [lst] (= L lst))
-(in-L-star? '(a))
-;; what about empty sequence
-;; is this too easy?
+(defn in-L-star? [lst] 
+  (if (empty? lst) 
+    true 
+    (if (= (first lst) 'a) 
+      (in-L-star? (rest lst)) 
+      false)))
+(in-L-star? '())
--- ps-2/ps2-tiegges-jan.clj
+++ ps-2/ps2-tiegges-jan.clj
@@ -7,10 +7,10 @@
 (defn sum-of-squares [x y] (+ (take-square x) (take-square y)))
 
 ;; Problem 3: define expressions `exp-13-1`, `exp-13-2`, `exp-13-3`, and `exp-13-4`
-(def exp-13-1 '(+ 4 4 5))
-(def exp-13-2 '(+ 12 1))
-(def exp-13-3 '(+ 1 1 1 1 1 1 1 1 1 1 1 1 1))
-(def exp-13-4 '(- 13 0))
+(def exp-13-1 '(- 13 0))
+(def exp-13-2 '(* 2 6.5))
+(def exp-13-3 '(/ 26 2))
+(def exp-13-4 '(Math/sqrt 169))
 
 ;; Problem 4: define a function `third`
 (defn third [x] (first (rest (rest x))))
@@ -23,37 +23,70 @@
 (defn compose [f g] (fn [x] (f (g x))))
 
 ;; Problem 6: define a function `first-two`
-(defn first-two [x] (take 2 x))
+(defn first-two [l] (list (first l) (first (rest l))))
 
 ;; Problem 7: define a function `remove-second`
-(defn remove-second [l] (concat (take 1 l) (drop 2 l)))
+(defn remove-second [l] (cons (first l) (rest (rest l))))
 
 ;; Problem 8: define a function `add-to-end`
-(defn add-to-end [lst x] (concat lst (list x)))
+(defn add-to-end [lst x]
+  (if (empty? lst)
+    (list x)
+    (cons (first lst) (add-to-end (rest lst) x))))
 
 ;; Problem 9: define a function `reverse`
 ;;           (Note that this funciton will overwrite a built in function. 
 ;;            This is okay.)
-(defn reverse-list [lst] (reduce conj '() lst))
+(defn reverse-list [lst]
+  (if (empty? lst)
+    '()
+    (concat (reverse-list (rest lst)) (list (first lst)))))
 
 ;; Problem 10: define a function `count-to-1`
-(defn count-to-1 [n] (reduce conj '() (take n (iterate inc 1))))
+(defn count-to-1 [n]
+  (if (<= n 1)
+    (list 1)
+    (cons n (count-to-1 (- n 1)))))
 
 ;; Problem 11: define a function `count-to-n`
-(defn get-max [lst] (last (sort lst)))
+(defn count-to-n [n] (reverse-list (count-to-1 n)))
 
 ;; Problem 12: define a function `get-max`
-(defn get-max [lst] (last (sort lst)))
+(defn get-max [lst]
+  (if (empty? lst)
+    nil
+    (if (empty? (rest lst))
+      (first lst)
+      (if (> (first lst) (get-max (rest lst)))
+        (first lst)
+        (get-max (rest lst))))))
 
 ;; Problem 13: define a function `greater-than-five?`
 (defn greater-than-five? [lst] (map (fn [x] (> x 5)) lst))
 
 ;; Problem 14: define a function `concat-three`
-(defn concat-three [x y z] (concat x y z))
+(defn concat-three [x y z]
+  (if (empty? x)
+    (if (empty? y)
+      z
+      (cons (first y) (concat-three x (rest y) z)))
+    (cons (first x) (concat-three (rest x) y z))))
 
 ;; Problem 15: define a function `sequence-to-power`
-(defn sequence-to-power [x n] (reduce concat '() (take n (iterate (fn [x] x) x))))
+(defn sequence-to-power [x n]
+  (if (empty? x)
+    '()
+    (if (= n 0)
+      '()
+      (if (= n 1)
+        x
+        (concat x (sequence-to-power x (- n 1)))))))
 
 ;; Problem 16: define a function `in-L-star?`
 (def L '(a))
-(defn in-L-star? [lst] (= L lst))
+(defn in-L-star? [lst]
+  (if (empty? lst)
+    true
+    (if (= (first lst) 'a)
+      (in-L-star? (rest lst))
+      false)))
